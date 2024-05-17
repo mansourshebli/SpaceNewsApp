6a91042fc312c5dc330ca6eefdc84fc19e5bf4a9
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2024-05-17 22:33:41 +0400

    Initial Commit

---
+++ SpaceNewsApp/SpaceNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsAppApp.swift
+//  SpaceNewsApp
+//
+//  Created by Mansour Mohammed Alshebli on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
